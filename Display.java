32f0d139d42d7ee91da4d4fb66a270f41600059e
westolu <lukeweston@whs.school.nz>
2021-07-01 15:15:44 +1200

    duguggy

--- Display.java
+++ Display.java
@@ -1,31 +1,19 @@
-/**
- * Write a description of class displaycanvas here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-
 import javax.swing.JFrame;
 import java.awt.Canvas;
-import java.awt.Graphics;
-import java.awt.Color;
 import java.awt.*;
 import javax.swing.JPanel;
 import javax.swing.*;
 
 public class Display extends JFrame {
-    
-    private JFrame frame;
     private Canvas canvas;
-    private JPanel Panel1 = new JPanel();
-    private JButton button = new JButton("speed up");
+    private final JPanel panel = new JPanel();
+    private final JButton button = new JButton("speed up");
+
+
+    private final int width;
+    private final int height;
     
-    
-    private String title;
-    private int width, height;
-    
-    public Display(String title, int width, int height){
-        this.title = title;
+    public Display(int width, int height){
         this.width = width;
         this.height = height;
         
@@ -44,8 +32,8 @@ public class Display extends JFrame {
         canvas.setPreferredSize(new Dimension(width, height));
         canvas.setMaximumSize(new Dimension(width, height));
         canvas.setMinimumSize(new Dimension(width, height));
-        Panel1.add(button);
-        this.getContentPane().add(Panel1);
+        panel.add(button);
+        this.getContentPane().add(panel);
         add(canvas);
         pack();
         this.toFront();
--- Preferences.java
+++ Preferences.java
@@ -5,8 +5,4 @@
 public class Preferences
 {
     public int[] vars = new int[] {10, 600, 600, 3000, 1, 300, 250};
-    public Preferences()
-    {
-        
-    }
 }
