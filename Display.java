3dc4d0c9dd60c62a9a069873ff328f2248b113e3
westolu <62079079+westolu@users.noreply.github.com>
2021-08-19 11:41:44 +1200

    commenting

--- Display.java
+++ Display.java
@@ -13,23 +13,23 @@ public class Display extends JFrame {
     private final int width;
     private final int height;
     
-    public Display(int width, int height){
+    public Display(int width, int height){  //take in two inputs, width and height
         this.width = width;
         this.height = height;
         
         createDisplay();
     }
     private void createDisplay(){
-        setSize(400, 400);
+        setSize(400, 400);          //initially set window to be 400*400
         
         setTitle("virus simulation");
-        setSize(width, height);
+        setSize(width, height); //resize to users input
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
         setLocationRelativeTo(null);
         
         canvas = new Canvas();
-        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setPreferredSize(new Dimension(width, height));  //set canvases max, min, and prefered size
         canvas.setMaximumSize(new Dimension(width, height));
         canvas.setMinimumSize(new Dimension(width, height));
         panel.add(button);
