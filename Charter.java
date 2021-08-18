bdcddb19cf804eb3c5ddf13703f76fdd19f7d451
westolu <62079079+westolu@users.noreply.github.com>
2021-08-18 13:22:57 +1200

    commenting on unclear code

--- Charter.java
+++ Charter.java
@@ -6,16 +6,16 @@ public class Charter {
     private final XYChart chart;
     private final SwingWrapper<XYChart> wrapper;
     public Charter(String chartTitle, String xAxisTitle, String yAxisTitle, String seriesName, double dataX, double dataY) {
-        xData = new ArrayList<>();
+        xData = new ArrayList<>(); //arraylists because they are move flexible than arrays (no defined capacity)
         yData = new ArrayList<>();
-        chart = QuickChart.getChart(chartTitle, xAxisTitle, yAxisTitle, seriesName, new double[]{dataX}, new double[]{dataY});
-        wrapper = new SwingWrapper<>(chart);
-        wrapper.displayChart();
+        chart = QuickChart.getChart(chartTitle, xAxisTitle, yAxisTitle, seriesName, new double[]{dataX}, new double[]{dataY});      //use library to make our own little graph
+        wrapper = new SwingWrapper<>(chart);    //put the graph into the swingwrapper 
+        wrapper.displayChart();             //display the graph on the wrapper
     }
-    public void addNewData(double newDataX, double newDataY) {
-        xData.add(newDataX);
+    public void addNewData(double newDataX, double newDataY) {  //this, when called, adds a new datapoint to the graph
+        xData.add(newDataX);                                    
         yData.add(newDataY);
-        chart.updateXYSeries("people infected", xData, yData, null);
-        wrapper.repaintChart();
+        chart.updateXYSeries("people infected", xData, yData, null);  //updates the new datapoints onto the graph
+        wrapper.repaintChart();         //repaint the graph
     }
 }
