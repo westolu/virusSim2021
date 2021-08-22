cd438dc538787c29a1e3c70b58513384a9adcf1b
westolu <62079079+westolu@users.noreply.github.com>
2021-08-23 11:48:11 +1200

    commenting unclear code, what each prefs.vars bit means

--- Preferences.java
+++ Preferences.java
@@ -6,7 +6,7 @@
 public class Preferences
 {
     public int[] vars = new int[] {10, 600, 600, 3000, 1, 300, 250, 100}; //I use this array to call upon and edit each preference without using lots of variables
-    public Preferences()
+    public Preferences()    //here, from start to end, this array goes (0) population, (1) width of world, (2) height of world, (3) cycles to run, (4) # of initially infected, (5) duration of infectivity, (6) duration of immunity, (7) infection chance
     {
         
     }
